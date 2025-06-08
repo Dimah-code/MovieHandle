b2a6ddf34259af7089447b0a0a5c98b02a0c3e99
Hammy <hammyghareghani@gmail.com>
2025-06-08 15:32:17 +0330

    Init Message

---
+++ Data/MovieHandleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MovieHandle.Models;
+
+namespace MovieHandle.Data
+{
+    public class MovieHandleContext : DbContext
+    {
+        public MovieHandleContext (DbContextOptions<MovieHandleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieHandle.Models.Movie> Movie { get; set; } = default!;
+    }
+}
